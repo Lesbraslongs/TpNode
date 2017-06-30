d0eb40324e6a7589e77e9eeac359cbaea69e7169
Arnaud <arnaud.lavallee44@gmail.com>
2017-06-30 14:26:06 +0200

    Display component work in progress.

--- front/src/app/model/Email.ts
+++ front/src/app/model/Email.ts
@@ -2,11 +2,24 @@
  * Created by Children on 29/06/2017.
  */
 export class Email {
+    id : number;
+    
+    name : string;
+    
+    firstname : string;
+    
+    domain : string;
+    
     constructor(
         id: number,
         name: string,
         firstname: string,
         domain: string
-    ) {}
+    ) {
+        this.id = id;
+        this.name = name;
+        this.firstname = firstname;
+        this.domain = domain    
+    }
 
 }
--- front/src/app/services/email.service.ts
+++ front/src/app/services/email.service.ts
@@ -4,7 +4,49 @@ import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import { Email } from '../model/Email';
 
+/**
+ * Created by Children on 29/06/2017.
+ * Angular service used to manage Email.
+ */
 @Injectable()
 export class EmailService {
+    private emailUrl = 'api/v1/emails';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
+ 
+    constructor(private http: Http) { }
+     
+    findAll() {
+        return this.http.get(this.emailUrl)
+                 .toPromise()
+                 .then(response => response.json().data as Email[]);
+    }
     
+    findById(id : number) {
+        const url = `${this.emailUrl}/${id}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Email);
+    }
+    
+    create(name: string, firstname: string, domain: string) {
+        return this.http
+          .post(this.emailUrl, JSON.stringify({name: name, firstname: firstname, domain: domain}), {headers: this.headers})
+          .toPromise()
+          .then(res => res.json().data as Email);
+    }
+    
+    update(email: Email) {
+        const url = `${this.emailUrl}/${email.id}`;
+        return this.http
+            .put(url, JSON.stringify(email), {headers: this.headers})
+            .toPromise()
+            .then(() => email);
+    }
+    
+    delete(id: number) {
+        const url = `${this.emailUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null);
+    }
 }
