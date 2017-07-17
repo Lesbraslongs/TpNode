339a8232293f8270df54414c354989b531fcd656
Amadou Fatty <fattyamadou.fatty@gmail.com>
2017-07-17 15:43:24 +0200

    npm bcrypt password

--- server/app/controllers/userCtrl.js
+++ server/app/controllers/userCtrl.js
@@ -1,51 +1,89 @@
-let User     = require('./../models/user.js');
-const db     = require('./../config/db').db;
+let User = require('./../models/user.js');
+const db = require('./../config/db').db;
 const config = require('./../config/config');
-const jwt    = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 class IndexCtrl {
 
-    constructor(){
-    }
+    constructor() {}
+
+
 
     registerUser(req, res) {
 
+        const saltRounds = 10;
+        const myPlaintextPassword = 's0///P4$$w0rD';
+        const someOtherPlaintextPassword = 'not_bacon';
+
         User.findOne({
             login: req.body._login
-        }, (err,foundUser) => {
-            if(foundUser){
+        }, (err, foundUser) => {
+            if (foundUser) {
                 res.json({success: false, message: 'A user with the same login already exists'});
-            }else{
+            } else {
+                //let hash = bcrypt.hashSync(req.body._password, 10);
+                bcrypt.hash(req.body._password, saltRounds, function(err, hash) {
+                    let user = new User({
+                        login: req.body._login,
+                        password: hash,
+                        admin: false
+                    });
 
-                let user = new User({
-                    login: req.body._login,
-                    password: req.body._password,
-                    admin: false
+                    user.save((err) => {
+                        if (err)
+                            throw err;
+                        res.json({success: true, message: 'You are now able to login with your credentials'});
+                    })
                 });
 
-                user.save( (err) => {
-                    if(err) throw err;
-                    res.json({ success: true , message: 'You are now able to login with your credentials'});
-                })
             }
         });
     }
 
     checkIfUserExists(req, res) {
 
+        const saltRounds = 10;
+        const myPlaintextPassword = 's0///P4$$w0rD';
+        const someOtherPlaintextPassword = 'not_bacon';
+
         User.findOne({
             login: req.body._login
-        }, function (err, user) {
+        }, function(err, user) {
 
-            if (err) throw err;
+            if (err)
+                throw err;
 
             if (!user) {
                 res.json({success: false, message: 'Authentication failed. User not found.'});
             } else if (user) {
+                /*bcrypt.compare(someOtherPlaintextPassword, req.body._password, function(err, rps) {
+                    // rps == false
+                    res.json({success: false, message: 'Authentication failed. Wrong password.'});
+                });*/
+
+                bcrypt.compare(user.password, req.body._password, function(err, rps){
+                    // rps == true
+                    // if user is found and password is right
+                    // create a token
+                    if (err) {
+                        res.json({success: false, message: 'Authentication failed. Wrong password.'});
+                    }
+
+                    // return the information including token as JSON
+                    res.json({
+                        success: true,
+                        message: 'Enjoy your token!',
+                        token: jwt.sign(user, config.secret)
+                    });
+
+
+                });
 
                 // check if password matches
-                if (user.password != req.body._password) {
-                    res.json({success: false, message: 'Authentication failed. Wrong password.'});
+                /*old version -> if (user.password != req.body._password)
+                if (!bcrypt.compareSync(user.password, hash)) {
+
                 } else {
 
                     // if user is found and password is right
@@ -55,9 +93,9 @@ class IndexCtrl {
                     res.json({
                         success: true,
                         message: 'Enjoy your token!',
-                        token: jwt.sign(user,config.secret)
+                        token: jwt.sign(user, config.secret)
                     });
-                }
+                }*/
             }
         });
     }
